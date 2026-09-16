8d2ca1bf9a297aea8246b062994a62c0dba4560a
maouai233 <maouai233@outlook.com>
2026-09-16 22:36:23 +0800

    newlib: textos: adopt default posix dir, with disabled posix_spawn

--- newlib/libc/posix/_isatty.c
+++ newlib/libc/posix/_isatty.c
@@ -1,3 +1,5 @@
+#ifndef HAVE_ISATTY
+
 /* isatty.c */
 
 /* Dumb implementation so programs will at least run.  */
@@ -19,3 +21,5 @@ _isatty (int fd)
   errno = ENOTTY;
   return 0;
 }
+
+#endif /* !HAVE_ISATTY */
