0d0a0dc55e95d8f9ee7285ffc35df308bcb1e116
maouai233 <maouai233@outlook.com>
2026-09-25 11:44:36 +0800

    newlib: textos: extract __syscall to bits/a-sc.h

--- newlib/libc/sys/textos/syscalls.c
+++ newlib/libc/sys/textos/syscalls.c
@@ -16,6 +16,7 @@
  */
 
 #include <_ansi.h>
+#include <bits/a-sc.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <poll.h>
@@ -36,24 +37,6 @@
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
-
-#define __syscall(num, a1, a2, a3, a4, a5, a6)                                 \
-  ({                                                                           \
-    register long __r0 __asm__("rax") = (long)(num);                           \
-    register long __r1 __asm__("rdi") = (long)(a1);                            \
-    register long __r2 __asm__("rsi") = (long)(a2);                            \
-    register long __r3 __asm__("rdx") = (long)(a3);                            \
-    register long __r4 __asm__("r10") = (long)(a4);                            \
-    register long __r5 __asm__("r8") = (long)(a5);                             \
-    register long __r6 __asm__("r9") = (long)(a6);                             \
-                                                                               \
-    __asm__ volatile(                                                          \
-      "syscall"                                                                \
-      : "+r"(__r0)                                                             \
-      : "r"(__r1), "r"(__r2), "r"(__r3), "r"(__r4), "r"(__r5), "r"(__r6)       \
-      : "memory", "rcx", "r11");                                               \
-    __r0;                                                                      \
-  })
 
 // According to the SysV ABI :
 // Returning from the syscall, register %rax contains the result of the
